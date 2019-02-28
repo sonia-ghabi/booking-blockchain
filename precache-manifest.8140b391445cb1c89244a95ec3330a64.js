70c144f31867ee3b0ba4278316d6a034105e2d8e
Sonia Ghabi <sonia.ghabi@gmail.com>
2019-02-28 20:59:15 +0800

    Updates

---
+++ precache-manifest.8140b391445cb1c89244a95ec3330a64.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b9f7e65f793cb7cc8000",
+    "url": "/booking-blockchain/static/js/runtime~main.b9f7e65f.js"
+  },
+  {
+    "revision": "d3f76a173433ad614bce",
+    "url": "/booking-blockchain/static/js/main.d3f76a17.chunk.js"
+  },
+  {
+    "revision": "0c698326dff1e00fc313",
+    "url": "/booking-blockchain/static/js/2.0c698326.chunk.js"
+  },
+  {
+    "revision": "d3f76a173433ad614bce",
+    "url": "/booking-blockchain/static/css/main.dda26559.chunk.css"
+  },
+  {
+    "revision": "d6c431a395e24c7a6261b08f346efa96",
+    "url": "/booking-blockchain/index.html"
+  }
+];
